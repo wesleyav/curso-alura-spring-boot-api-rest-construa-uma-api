b99b82be101ea9375ac2f12adbf32407a9633346
Wesley <wesley.contato@hotmail.com>
2020-10-26 06:34:17 -0300

    Cadastrando tópicos

--- forum/src/main/java/br/com/alura/forum/modelo/Topico.java
+++ forum/src/main/java/br/com/alura/forum/modelo/Topico.java
@@ -29,6 +29,19 @@ public class Topico {
 	private Curso curso;
 	@OneToMany(mappedBy = "topico")
 	private List<Resposta> respostas = new ArrayList<>();
+	
+	public Topico() {
+		
+	}
+	
+	public Topico(String titulo, String mensagem, Curso curso) {
+		super();
+		this.titulo = titulo;
+		this.mensagem = mensagem;
+		this.curso = curso;
+	}
+
+
 
 	@Override
 	public int hashCode() {
